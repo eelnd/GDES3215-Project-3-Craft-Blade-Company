08504dd4a35b259b04cfefbda3a94263e27b90f7
eelnd <115965742+eelnd@users.noreply.github.com>
2023-12-04 15:11:57 -0500

    Initial Commit

---
+++ js/main.js
@@ -0,0 +1,31 @@
+/* EMAIL FORM HANDLING */
+
+$('#email-form').submit(function(e){
+  // do not submit form
+  e.preventDefault();
+  // save value if the form field is not blank or the default
+  if( $('#email').val() !== '' && $('#email').val() !== 'Email' ){
+    // store value added to field in a variable
+    let val = $('#email').val();
+    // add email address to message
+    $('.form-confirmation-message span').text(val);
+    // show message (initially hidden with "hide" class)
+    $('.form-confirmation-message').removeClass('hide');
+    // hide input
+    $('.form-input').addClass('hide');
+  }
+})
+
+$(document).ready(function() {
+  function moveText() {
+      $(".banner p").animate({ "margin-left": "-99%" }, 15000, "linear", function() {
+          // Reset the position once the animation is complete
+          $(this).css("margin-left", "100%");
+          // Call the function again for continuous looping
+          moveText();
+      });
+  }
+
+  // Call the function to start the animation
+  moveText();
+});

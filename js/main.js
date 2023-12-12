3c433348d6f9d60961d3a9562b085c46210eaa4a
eelnd <115965742+eelnd@users.noreply.github.com>
2023-12-12 18:23:34 -0500

    mobile
    created mobile layout

--- js/main.js
+++ js/main.js
@@ -29,3 +29,8 @@ $(document).ready(function() {
   // Call the function to start the animation
   moveText();
 });
+
+function toggleMenu() {
+  const navLinks = document.querySelector('.nav-links');
+  navLinks.style.display = (navLinks.style.display === 'flex') ? 'none' : 'flex';
+}
